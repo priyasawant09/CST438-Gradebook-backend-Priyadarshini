fec11ebbae4f1c4cbfb376cb8ad4142fb40b065d
priya <sawantpriyadarshini28@gmail.com>
2023-10-31 09:55:46 -0700

    Assignment 6 login

---
+++ cst438_gradebook/src/main/java/com/cst438/PasswordEncoder.java
@@ -0,0 +1,15 @@
+package com.cst438;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoder {
+
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String password = "pass";
+        String encrpted_password = encoder.encode(password);
+        System.out.println(encrpted_password);
+    }
+
+
+}
